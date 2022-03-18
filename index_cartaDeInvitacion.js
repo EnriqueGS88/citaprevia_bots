b5d25e85b064683f86abb815897c14bc3df816f1
EnriqueGS88 <enrique.gzs@gmail.com>
2022-03-18 09:37:51 +0100

    bot to get appointment for an invitation letter

--- index_cartaDeInvitacion.js
+++ index_cartaDeInvitacion.js
@@ -1,4 +1,77 @@
+const puppeteer = require('puppeteer');
+const urls = require('./urls');
+require('dotenv').config();
 
 
-const selectorAcceptButton = 'input[id="btnAceptar"]';
+async function submitTimesheet() {
+
+    const browser = await puppeteer.launch(
+        {
+            headless: false,
+            defaultViewport: {
+                width: 1400,
+                height: 900,
+                deviceScaleFactor: 1,
+            },
+            slowMo: 70
+        }    
+    );
+
+    const selectorAcceptButton = 'input[id="btnAceptar"]';
+    const selectorOfficeMenu = 'select[id="sede"]';
+    const selectorTramiteMenu = 'select[id="tramiteGrupo[0]"]';
+    const selectorEntrarButton = 'input[id="btnEntrar"]';
+    const selectorPassportButton = 'input[value="PASAPORTE"]';
+    const selectorPassportField = 'input[id="txtIdCitado"]';
+    const selectorNameField = 'input[id="txtDesCitado"]';
+    const selectorEnviarButton = 'input[id="btnEnviar"]';
+    
+    const PASSPORT_NUMBER = process.env.PASSPORT_NUMBER;
+    const NAME = process.env.NAME;
+    
+    // Start on Page 1
+    const page = await browser.newPage();
+    await page.goto( urls.cartaDeInvitacion );
+    await page.waitForTimeout( 1000 );
+
+    await page.waitForSelector( selectorAcceptButton );
+
+    await page.select( selectorOfficeMenu, "48" );
+    await page.waitForTimeout( 3000 );
+
+    await page.select( selectorTramiteMenu, "4037" );
+    await page.waitForTimeout( 2000 );
+
+    await page.click( selectorAcceptButton );
+    await page.waitForTimeout( 2000 );
+
+    // Move to Page 2
+    await page.click( selectorEntrarButton );
+    await page.waitForTimeout( 2000 );
+
+    // Move to Page 3
+    await page.click( selectorPassportButton );
+    await page.waitForTimeout( 1000 );
+
+    await page.focus( selectorPassportField );
+    await page.keyboard.type( PASSPORT_NUMBER );
+    await page.waitForTimeout( 1000 );
+    
+    await page.focus( selectorNameField );
+    await page.keyboard.type( NAME );
+    await page.waitForTimeout( 1000 );
+
+    await page.click( selectorEnviarButton );
+    await page.waitForTimeout( 2000 );
+
+    await page.click( selectorEnviarButton );
+    await page.waitForTimeout( 29000 );
+
+    // await page.close();
+
+
+}
+
+submitTimesheet();
+
 
--- urls.js
+++ urls.js
@@ -1,5 +1,6 @@
+module.exports = {
 
+    cartaDeInvitacion:'https://sede.administracionespublicas.gob.es/icpplustieb/citar?p=8&locale=es',
+    dgt: 'https://sedeclave.dgt.gob.es/WEB_NCIT_CONSULTA/solicitarCita.faces'
 
-
-const cartaDeInvitacionURL = 'https://sede.administracionespublicas.gob.es/icpplustie/icpplustieb/acOpcDirect';
-
+}
